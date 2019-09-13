5ad19a23d704abe06af598d86d1ef23cdbebad71
Patrick Henninger <patrick.henninger@contagt.com>
2019-09-13 18:22:03 +0200

    Added getColor helper, JSDoc,

--- src/js/L.Marker.ColorArrow.js
+++ src/js/L.Marker.ColorArrow.js
@@ -6,18 +6,44 @@ L.Marker.ColorArrow = L.Marker.extend({
         color: 'red',
     },
 
+    /**
+     * Sets the marker color to the given RGBA color.
+     * @param {number} r Red value (0-255)
+     * @param {number} g Green value (0-255)
+     * @param {number} b Blue value (0-255)
+     * @param {number} a Alpha value (0-1)
+     */
     setRGBA(r, g, b, a = 1) {
         this.options.color = 'rgba('+r+','+g+','+b+','+a+')';
         this.setColor();
     },
 
+    /**
+     * Sets the marker color to the given HSVA color
+     * @param {number} h Hue value (0-1)
+     * @param {number} s Saturation value (0-1)
+     * @param {number} v Value value (0-1)
+     * @param {number} a Alpha value (0-1)
+     */
     setHSVA(h, s = 1, v = 1, a = 1) {
         const rgb = this._HSVtoRGB(h, s, v);
         this.setRGBA(rgb.r, rgb.g, rgb.b, a);
     },
 
+    /**
+     * Sets the marker color to the given CSS color.
+     * @param {string} color 
+     */
     setColor(color) {
         this._icon.style['border-bottom-color'] = this.options.color;
+    },
+
+    /**
+     * Returns the current color.
+     * @returns {string}
+     */
+    getColor() {
+        return this.options.color;
     },
 
     /**
@@ -58,6 +84,7 @@ L.Marker.ColorArrow = L.Marker.extend({
 });
 
 L.Marker.ColorArrow.addInitHook(function () {
+    /* Set initial marker color on add since the icon does only exist then */
     this.on('add', event => {
         this.setColor(this.options.color);
     });
